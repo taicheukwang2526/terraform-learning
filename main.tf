2fdf8db0d1311235c538801019824ab6578b84c3
taicheukwang2526 <250214264@stu.vtc.edu.hk>
2026-06-22 09:52:10 +0000

    Add Terraform variables and outputs

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+    }
+  }
+}
+
 variable "file_name" {
   default = "hello.txt"
 }
@@ -9,4 +17,12 @@ variable "message" {
 resource "local_file" "hello" {
   filename = var.file_name
   content  = var.message
+}
+
+output "file_path" {
+  value = local_file.hello.filename
+}
+
+output "file_content" {
+  value = local_file.hello.content
 }
